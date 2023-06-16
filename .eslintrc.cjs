b1134dea96e504cd4b7c3daf2035a7f0b20fc96c
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-06-16 14:57:19 -0700

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: "@antfu",
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
 };
