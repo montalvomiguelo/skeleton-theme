e62e53597c95cb4400e4adf3da521a17dadb623b
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-09-30 12:13:43 -0400

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
