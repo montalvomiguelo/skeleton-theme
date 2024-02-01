37a7344121d363b3c7d2fde9286b125a4d73fd18
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2024-01-31 21:05:14 -0500

    Update config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './assets/*.{liquid,js,mjs}',
-    '!./assets/*.min.{js,mjs}',
-    './config/*.json',
-    './layout/*.liquid',
-    './sections/*.liquid',
-    './snippets/*.liquid',
-    './templates/**/*.{liquid,json}',
-  ],
+  content: ['./layout/*.liquid', './sections/*.liquid', './snippets/*.liquid', './templates/**/*.{liquid,json}'],
   theme: {
     extend: {},
   },
