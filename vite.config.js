64102710021a58d21c228ada88a3eb538200e340
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2025-01-25 15:49:11 -0600

    Tailwind v4

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import shopify from 'vite-plugin-shopify';
 import importMaps from 'vite-plugin-shopify-import-maps';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     shopify({ snippetFile: 'vite.liquid', versionNumbers: true }),
+    tailwindcss(),
     importMaps({ bareModules: true }),
   ],
   build: {
