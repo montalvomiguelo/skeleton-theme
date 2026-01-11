1e557c1f81aa1b0502142fc570b6fbf8ede5eecd
miguel <miguel.montalvo@meetdomaine.com>
2026-01-11 00:52:04 -0600

    Update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -2,6 +2,8 @@
 export default {
   printWidth: 100,
   singleQuote: true,
+  semi: false,
+  trailingComma: 'es5',
   plugins: ['@shopify/prettier-plugin-liquid', 'prettier-plugin-tailwindcss'],
   tailwindStylesheet: './frontend/entrypoints/theme.css',
   overrides: [
@@ -13,4 +15,4 @@ export default {
       },
     },
   ],
-};
+}
