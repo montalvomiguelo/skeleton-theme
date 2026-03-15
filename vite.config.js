750100cf9cae7e763969446e929cb9690a38bd84
Miguel Montalvo <miguel.montalvo@meetdomaine.com>
2026-03-14 19:03:03 -0600

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [shopify({ tunnel: true }), tailwindcss()],
+  plugins: [shopify(), tailwindcss()],
   build: {
     rollupOptions: {
       output: {
